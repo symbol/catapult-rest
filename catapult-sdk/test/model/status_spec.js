5f1e9a8711aa4c5f74a0f70b2ac0c57aad886a52
Jaguar0625 <jaguar0625@127.0.0.1>
2019-01-03 14:20:27 -0500

    update status.js with cow status codes

--- catapult-sdk/test/model/status_spec.js
+++ catapult-sdk/test/model/status_spec.js
@@ -38,7 +38,7 @@ describe('status', () => {
 			expect(status.toString(0x80430005)).to.equal('Failure_Core_Insufficient_Balance');
 			expect(status.toString(0x80450101)).to.equal('Failure_Extension_Partial_Transaction_Cache_Prune');
 			expect(status.toString(0x81480007)).to.equal('Failure_Hash_Exists');
-			expect(status.toString(0x804C0010)).to.equal('Failure_Lock_Invalid_Duration');
+			expect(status.toString(0x80520009)).to.equal('Failure_LockSecret_Invalid_Duration');
 			expect(status.toString(0x804D0002)).to.equal('Failure_Mosaic_Invalid_Name');
 			expect(status.toString(0x80550003)).to.equal('Failure_Multisig_Modify_Redundant_Modifications');
 			expect(status.toString(0x804E0002)).to.equal('Failure_Namespace_Invalid_Name');
