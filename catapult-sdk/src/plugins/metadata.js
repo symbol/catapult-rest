bb2addfc6f44b18c229269a71ee36957c5882979
Roger Hernandez <rogerernandez@hotmail.com>
2019-08-28 16:36:15 +0200

    Enabled metadata and mosaicRestriction REST plugin schemas

--- catapult-sdk/src/plugins/metadata.js
+++ catapult-sdk/src/plugins/metadata.js
@@ -39,21 +39,21 @@ const metadataPlugin = {
 		builder.addTransactionSupport(EntityType.accountMetadata, {
 			targetPublicKey: ModelType.binary,
 			scopedMetadataKey: ModelType.uint64,
-			value: ModelType.string
+			value: ModelType.binary
 		});
 
 		builder.addTransactionSupport(EntityType.mosaicMetadata, {
 			targetPublicKey: ModelType.binary,
 			scopedMetadataKey: ModelType.uint64,
 			targetMosaicId: ModelType.uint64,
-			value: ModelType.string
+			value: ModelType.binary
 		});
 
 		builder.addTransactionSupport(EntityType.namespaceMetadata, {
 			targetPublicKey: ModelType.binary,
 			scopedMetadataKey: ModelType.uint64,
 			targetNamespaceId: ModelType.uint64,
-			value: ModelType.string
+			value: ModelType.binary
 		});
 	},
 
