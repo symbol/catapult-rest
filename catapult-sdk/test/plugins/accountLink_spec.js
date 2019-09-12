5d2621bad002434e8c222c502559cc138d492ae5
Martín Ayora <martin@nemeurope.eu>
2019-09-12 11:36:25 +0200

    Fixed accountLink schema

--- catapult-sdk/test/plugins/accountLink_spec.js
+++ catapult-sdk/test/plugins/accountLink_spec.js
@@ -41,7 +41,7 @@ describe('account link plugin', () => {
 
 			// - accountLink
 			expect(Object.keys(modelSchema.accountLink).length).to.equal(Object.keys(modelSchema.transaction).length + 1);
-			expect(modelSchema.accountLink).to.contain.all.keys(['linkedAccountKey']);
+			expect(modelSchema.accountLink).to.contain.all.keys(['remotePublicKey']);
 		});
 	});
 
@@ -67,17 +67,17 @@ describe('account link plugin', () => {
 		const codec = getCodecs()[EntityType.accountLink];
 
 		describe('supports account link transaction', () => {
-			const linkedAccountKey = Buffer.of(
+			const remotePublicKey = Buffer.of(
 				0x77, 0xBE, 0xE1, 0xCA, 0xD0, 0x8E, 0x6E, 0x48, 0x95, 0xE8, 0x18, 0xB2, 0x7B, 0xD8, 0xFA, 0xC9,
 				0x47, 0x0D, 0xB8, 0xFD, 0x2D, 0x81, 0x47, 0x6A, 0xC5, 0x61, 0xA4, 0xCE, 0xE1, 0x81, 0x40, 0x83
 			);
 			test.binary.test.addAll(codec, 32 + 1, () => ({
 				buffer: Buffer.concat([
-					linkedAccountKey,
+					remotePublicKey,
 					Buffer.of(0x01)
 				]),
 				object: {
-					linkedAccountKey,
+					remotePublicKey,
 					linkAction: 0x01
 				}
 			}));
