ee9d0793323d7aa4d2f2cb5d02d2199c901c71d4
Roger Hernandez <rogerernandez@hotmail.com>
2020-05-29 16:40:36 +0200

    Renamed AccountKeyLink's remotePublicKey to linkedPublicKey

--- catapult-sdk/src/plugins/accountLink.js
+++ catapult-sdk/src/plugins/accountLink.js
@@ -32,7 +32,7 @@ const constants = { sizes };
 const accountLinkPlugin = {
 	registerSchema: builder => {
 		builder.addTransactionSupport(EntityType.accountLink, {
-			remotePublicKey: ModelType.binary
+			linkedPublicKey: ModelType.binary
 		});
 
 		builder.addTransactionSupport(EntityType.nodeKeyLink, {
@@ -52,13 +52,13 @@ const accountLinkPlugin = {
 		codecBuilder.addTransactionSupport(EntityType.accountLink, {
 			deserialize: parser => {
 				const transaction = {};
-				transaction.remotePublicKey = parser.buffer(constants.sizes.signerPublicKey);
+				transaction.linkedPublicKey = parser.buffer(constants.sizes.signerPublicKey);
 				transaction.linkAction = parser.uint8();
 				return transaction;
 			},
 
 			serialize: (transaction, serializer) => {
-				serializer.writeBuffer(transaction.remotePublicKey);
+				serializer.writeBuffer(transaction.linkedPublicKey);
 				serializer.writeUint8(transaction.linkAction);
 			}
 		});
