345099a0c68ce8e5de958b1763c6b38a84597d88
Martin Ayora <martin.ayora@nemstudios.io>
2020-07-06 13:45:18 +0200

    Updated route tests

--- rest/test/routes/accountRoutes_spec.js
+++ rest/test/routes/accountRoutes_spec.js
@@ -18,72 +18,370 @@
  * along with Catapult.  If not, see <http://www.gnu.org/licenses/>.
  */
 
+const { MockServer } = require('./utils/routeTestUtils');
 const { test } = require('./utils/routeTestUtils');
 const accountRoutes = require('../../src/routes/accountRoutes');
+const routeResultTypes = require('../../src/routes/routeResultTypes');
+const routeUtils = require('../../src/routes/routeUtils');
 const catapult = require('catapult-sdk');
+const { expect } = require('chai');
+const sinon = require('sinon');
 
 const { address } = catapult.model;
 const { convert } = catapult.utils;
-const { addresses, publicKeys } = test.sets;
 
 describe('account routes', () => {
-	describe('get by account', () => {
-		const addGetTests = (key, ids, parsedIds, validBody, invalidBody, errorMessage) => {
-			test.route.document.addGetPostDocumentRouteTests(accountRoutes.register, {
-				routes: { singular: '/accounts/:accountId', plural: '/accounts' },
-				inputs: {
-					valid: { object: { accountId: ids[0] }, parsed: [{ [key]: parsedIds[0] }], printable: ids[0] },
-					validMultiple: { object: validBody, parsed: parsedIds.map(parsedId => ({ [key]: parsedId })) },
-					invalid: { object: { accountId: '12345' }, error: 'accountId has an invalid format' },
-					invalidMultiple: {
-						object: invalidBody,
-						error: errorMessage
+	const testAddress = 'NAR3W7B4BCOZSZMFIZRYB3N5YGOUSWIYJCJ6HDA';
+	const testPublicKey = '7DE16AEDF57EB9561D3E6EFA4AE66F27ABDA8AEC8BC020B6277360E31619DCE7';
+
+	describe('GET', () => {
+		describe('accounts', () => {
+			const testMosaicId = 'ABCDEF0123456789';
+
+			const emptyPageSample = {
+				data: [],
+				pagination: {
+					pageNumber: 1,
+					pageSize: 10,
+					totalEntries: 0,
+					totalPages: 0
+				}
+			};
+
+			const pageSample = {
+				data: [
+					{
+						id: 'random1',
+						account: {
+							address: '',
+							addressHeight: '',
+							publicKey: '',
+							publicKeyHeight: '',
+							supplementalPublicKeys: {},
+							importance: '',
+							importanceHeight: '',
+							activityBuckets: [],
+							mosaics: []
+						}
+					},
+					{
+						id: 'random2',
+						account: {
+							address: '',
+							addressHeight: '',
+							publicKey: '',
+							publicKeyHeight: '',
+							supplementalPublicKeys: {},
+							importance: '',
+							importanceHeight: '',
+							activityBuckets: [],
+							mosaics: []
+						}
 					}
-				},
-				dbApiName: 'accountsByIds',
-				type: 'accountWithMetadata',
-				config: { transactionStates: [] }
+				],
+				pagination: {
+					pageNumber: 1,
+					pageSize: 10,
+					totalEntries: 2,
+					totalPages: 1
+				}
+			};
+
+			const dbAccountsFake = sinon.fake(accountAddress =>
+				(accountAddress ? Promise.resolve(emptyPageSample) : Promise.resolve(pageSample)));
+
+			const services = {
+				config: {
+					pageSize: {
+						min: 10,
+						max: 100,
+						default: 20
+					}
+				}
+			};
+
+			const mockServer = new MockServer();
+			const db = { accounts: dbAccountsFake };
+			accountRoutes.register(mockServer.server, db, services);
+
+			beforeEach(() => {
+				mockServer.resetStats();
+				dbAccountsFake.resetHistory();
 			});
-		};
 
-		describe('by address', () =>
-			addGetTests(
-				'address',
-				addresses.valid,
-				addresses.valid.map(address.stringToAddress),
-				{ addresses: addresses.valid },
-				{ addresses: [addresses.valid[0], '12345', addresses.valid[1]] },
-				'element in array addresses has an invalid format'
-			));
+			const route = mockServer.getRoute('/accounts').get();
 
-		describe('by publicKey', () =>
-			addGetTests(
-				'publicKey',
-				publicKeys.valid,
-				publicKeys.valid.map(convert.hexToUint8),
-				{ publicKeys: publicKeys.valid },
-				{ publicKeys: [publicKeys.valid[0], '12345', publicKeys.valid[1]] },
-				'element in array publicKeys has an invalid format'
-			));
 
-		it('does not support publicKeys and addresses provided at the same time', () => {
-			// Arrange:
-			const keyGroups = [];
-			const db = test.setup.createCapturingDb('accountsByIds', keyGroups, [{ value: 'this is nonsense' }]);
+			it('parses and forwards paging options', () => {
+				// Arrange:
+				const pagingBag = 'fakePagingBagObject';
+				const paginationParser = sinon.stub(routeUtils, 'parsePaginationArguments').returns(pagingBag);
+				const req = { params: {} };
 
-			// Act:
-			const registerRoutes = accountRoutes.register;
-			const errorMessage = 'publicKeys and addresses cannot both be provided';
-			return test.route.executeThrows(
-				registerRoutes,
-				'/accounts',
-				'post',
-				{ addresses: addresses.valid, publicKeys: publicKeys.valid },
-				db,
-				{ transactionStates: [] },
-				errorMessage,
-				409
-			);
+				// Act:
+				return mockServer.callRoute(route, req).then(() => {
+					// Assert:
+					expect(paginationParser.firstCall.args[0]).to.deep.equal(req.params);
+					expect(paginationParser.firstCall.args[2]).to.deep.equal({
+						id: 'objectId',
+						balance: 'uint64'
+					});
+
+					expect(dbAccountsFake.calledOnce).to.equal(true);
+					expect(dbAccountsFake.firstCall.args[2]).to.deep.equal(pagingBag);
+					paginationParser.restore();
+				});
+			});
+
+			it('allowed sort fields are taken into account', () => {
+				// Arrange:
+				const paginationParserSpy = sinon.spy(routeUtils, 'parsePaginationArguments');
+				const expectedAllowedSortFields = { id: 'objectId', balance: 'uint64' };
+
+				// Act:
+				return mockServer.callRoute(route, { params: {} }).then(() => {
+					// Assert:
+					expect(paginationParserSpy.calledOnce).to.equal(true);
+					expect(paginationParserSpy.firstCall.args[2]).to.deep.equal(expectedAllowedSortFields);
+					paginationParserSpy.restore();
+				});
+			});
+
+			it('returns empty page if no accounts found', () => {
+				// Arrange:
+				const req = { params: { address: testAddress } };
+
+				// Act:
+				return mockServer.callRoute(route, req).then(() => {
+					// Assert:
+					expect(dbAccountsFake.calledOnce).to.equal(true);
+
+					expect(mockServer.send.firstCall.args[0]).to.deep.equal({
+						payload: emptyPageSample,
+						type: routeResultTypes.account,
+						structure: 'page'
+					});
+					expect(mockServer.next.calledOnce).to.equal(true);
+				});
+			});
+
+			it('forwards query without address if not provided', () => {
+				// Arrange:
+				const req = { params: {} };
+
+				// Act:
+				return mockServer.callRoute(route, req).then(() => {
+					// Assert:
+					expect(dbAccountsFake.calledOnce).to.equal(true);
+					expect(dbAccountsFake.firstCall.args[0]).to.deep.equal(undefined);
+
+					expect(mockServer.next.calledOnce).to.equal(true);
+				});
+			});
+
+			it('forwards query without mosaicId if not provided', () => {
+				// Arrange:
+				const req = { params: {} };
+
+				// Act:
+				return mockServer.callRoute(route, req).then(() => {
+					// Assert:
+					expect(dbAccountsFake.calledOnce).to.equal(true);
+					expect(dbAccountsFake.firstCall.args[1]).to.deep.equal(undefined);
+
+					expect(mockServer.next.calledOnce).to.equal(true);
+				});
+			});
+
+			it('forwards address', () => {
+				// Arrange:
+				const req = { params: { address: testAddress } };
+
+				// Act:
+				return mockServer.callRoute(route, req).then(() => {
+					// Assert:
+					expect(dbAccountsFake.calledOnce).to.equal(true);
+					expect(dbAccountsFake.firstCall.args[0]).to.deep.equal(address.stringToAddress(testAddress));
+
+					expect(mockServer.next.calledOnce).to.equal(true);
+				});
+			});
+
+			it('forwards mosaicId', () => {
+				// Arrange:
+				const req = { params: { mosaicId: testMosaicId } };
+
+				// Act:
+				return mockServer.callRoute(route, req).then(() => {
+					// Assert:
+					expect(dbAccountsFake.calledOnce).to.equal(true);
+					expect(dbAccountsFake.firstCall.args[1]).to.deep.equal([0x23456789, 0xABCDEF01]);
+
+					expect(mockServer.next.calledOnce).to.equal(true);
+				});
+			});
+
+			it('returns page with results', () => {
+				// Arrange:
+				const req = { params: {} };
+
+				// Act:
+				return mockServer.callRoute(route, req).then(() => {
+					// Assert:
+					expect(dbAccountsFake.calledOnce).to.equal(true);
+					expect(dbAccountsFake.firstCall.args[0]).to.deep.equal(undefined);
+					expect(dbAccountsFake.firstCall.args[1]).to.deep.equal(undefined);
+
+					expect(mockServer.send.firstCall.args[0]).to.deep.equal({
+						payload: pageSample,
+						type: routeResultTypes.account,
+						structure: 'page'
+					});
+					expect(mockServer.next.calledOnce).to.equal(true);
+				});
+			});
+
+			it('throws error if address is invalid', () => {
+				// Arrange:
+				const req = { params: { address: 'AB12345' } };
+
+				// Act + Assert:
+				expect(() => mockServer.callRoute(route, req)).to.throw('address has an invalid format');
+			});
+
+			it('throws error if mosaicId is invalid', () => {
+				// Arrange:
+				const req = { params: { mosaicId: 'AB12345' } };
+
+				// Act + Assert:
+				expect(() => mockServer.callRoute(route, req)).to.throw('mosaicId has an invalid format');
+			});
+		});
+
+		describe('by accountId', () => {
+			describe('by address', () => {
+				test.route.document.addGetDocumentRouteTests(accountRoutes.register, {
+					route: '/accounts/:accountId',
+					inputs: {
+						valid: {
+							object: { accountId: testAddress },
+							parsed: [[{ address: address.stringToAddress(testAddress) }]],
+							printable: testAddress
+						},
+						invalid: {
+							object: { accountId: '12345' },
+							error: 'accountId has an invalid format'
+						}
+					},
+					dbApiName: 'accountsByIds',
+					type: routeResultTypes.account
+				});
+			});
+
+			describe('by publicKey', () => {
+				test.route.document.addGetDocumentRouteTests(accountRoutes.register, {
+					route: '/accounts/:accountId',
+					inputs: {
+						valid: {
+							object: { accountId: testPublicKey },
+							parsed: [[{ publicKey: convert.hexToUint8(testPublicKey) }]],
+							printable: testPublicKey
+						},
+						invalid: {
+							object: { accountId: '12345' },
+							error: 'accountId has an invalid format'
+						}
+					},
+					dbApiName: 'accountsByIds',
+					type: routeResultTypes.account
+				});
+			});
+		});
+	});
+
+	describe('POST', () => {
+		describe('accounts', () => {
+			const fakeAccounts = [{ id: '', account: { address: '' } }];
+			const dbAccountsByIds = sinon.fake.resolves(fakeAccounts);
+			const mockServer = new MockServer();
+			const db = { accountsByIds: dbAccountsByIds };
+			accountRoutes.register(mockServer.server, db, {});
+
+			const route = mockServer.getRoute('/accounts').post();
+
+			beforeEach(() => {
+				mockServer.resetStats();
+				dbAccountsByIds.resetHistory();
+			});
+
+			it('throws if both publicKeys and addresses are provided', () => {
+				// Arrange
+				const req = { params: { addresses: [], publicKeys: [] } };
+
+				// Act + Assert:
+				expect(() => mockServer.callRoute(route, req)).to.throw('publicKeys and addresses cannot both be provided');
+			});
+
+			const runParseArgumentAsArrayParamTest = (paramValues, paramName, parserName) => {
+				// Arrange
+				const req = { params: { [paramName]: paramValues } };
+				const parseArgumentsAsArraySpy = sinon.spy(routeUtils, 'parseArgumentAsArray');
+
+				// Act:
+				return mockServer.callRoute(route, req).then(() => {
+					// Assert:
+					expect(parseArgumentsAsArraySpy.calledOnceWith(
+						{ [paramName]: paramValues },
+						paramName,
+						parserName
+					)).to.equal(true);
+					parseArgumentsAsArraySpy.restore();
+				});
+			};
+
+			it('calls parseArgumentAsArray with correct parser for addresses', () =>
+				runParseArgumentAsArrayParamTest([testAddress, testAddress], 'addresses', 'address'));
+
+			it('calls parseArgumentAsArray with correct parser for public keys', () =>
+				runParseArgumentAsArrayParamTest([testPublicKey, testPublicKey], 'publicKeys', 'publicKey'));
+
+			describe('calls correct accounts retriever with correct params', () => {
+				it('adress params', () => {
+					// Arrange
+					const req = { params: { addresses: [testAddress] } };
+
+					// Act:
+					return mockServer.callRoute(route, req).then(() => {
+						// Assert:
+						expect(dbAccountsByIds.calledOnce).to.equal(true);
+						expect(dbAccountsByIds.firstCall.args[0]).to.deep.equal([{ address: address.stringToAddress(testAddress) }]);
+
+						expect(mockServer.send.firstCall.args[0]).to.deep.equal({
+							payload: fakeAccounts,
+							type: routeResultTypes.account
+						});
+						expect(mockServer.next.calledOnce).to.equal(true);
+					});
+				});
+
+				it('publicKey params', () => {
+					// Arrange
+					const req = { params: { publicKeys: [testPublicKey] } };
+
+					// Act:
+					return mockServer.callRoute(route, req).then(() => {
+						// Assert:
+						expect(dbAccountsByIds.calledOnce).to.equal(true);
+						expect(dbAccountsByIds.firstCall.args[0]).to.deep.equal([{ publicKey: convert.hexToUint8(testPublicKey) }]);
+
+						expect(mockServer.send.firstCall.args[0]).to.deep.equal({
+							payload: fakeAccounts,
+							type: routeResultTypes.account
+						});
+						expect(mockServer.next.calledOnce).to.equal(true);
+					});
+				});
+			});
 		});
 	});
 });
