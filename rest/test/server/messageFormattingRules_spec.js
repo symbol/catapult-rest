109524bb02dae3990fc9e692581076c5078a0eff
Martin Ayora <martin.ayora@nemstudios.io>
2020-08-03 11:48:14 +0200

    Added missing tests

--- rest/test/server/messageFormattingRules_spec.js
+++ rest/test/server/messageFormattingRules_spec.js
@@ -75,6 +75,17 @@ describe('message formatting rules', () => {
 		expect(result).to.equal('catapult');
 	});
 
+	it('can format uint type', () => {
+		// Arrange:
+		const object = 12345678;
+
+		// Act:
+		const result = formattingRules[ModelType.uint](object);
+
+		// Assert:
+		expect(result).to.deep.equal(12345678);
+	});
+
 	it('can format uint16 type', () => {
 		// Arrange:
 		const object = 56;
@@ -95,6 +106,17 @@ describe('message formatting rules', () => {
 
 		// Assert:
 		expect(result).to.equal('8589934593');
+	});
+
+	it('can format int type', () => {
+		// Arrange:
+		const object = 12345678;
+
+		// Act:
+		const result = formattingRules[ModelType.int](object);
+
+		// Assert:
+		expect(result).to.deep.equal(12345678);
 	});
 
 	it('can format uint64HexIdentifier type', () => {
