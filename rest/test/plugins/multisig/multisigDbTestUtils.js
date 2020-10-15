15922d068158aa49a6e62feb4a2a6681e8762665
Steven Liu <xian.f.liu@gmail.com>
2020-10-15 14:31:01 +0100

    Updated License (#500)

--- rest/test/plugins/multisig/multisigDbTestUtils.js
+++ rest/test/plugins/multisig/multisigDbTestUtils.js
@@ -1,6 +1,7 @@
 /*
- * Copyright (c) 2016-present,
- * Jaguar0625, gimre, BloodyRookie, Tech Bureau, Corp. All rights reserved.
+ * Copyright (c) 2016-2019, Jaguar0625, gimre, BloodyRookie, Tech Bureau, Corp.
+ * Copyright (c) 2020-present, Jaguar0625, gimre, BloodyRookie.
+ * All rights reserved.
  *
  * This file is part of Catapult.
  *
