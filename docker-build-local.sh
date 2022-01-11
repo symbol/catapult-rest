d0e7edee32a9222094f0a4299ba7cb5317b7b933
gimre <g.imre@protonmail.com>
2022-01-11 17:20:39 +0100

    move to monorepo

--- docker-build-local.sh
+++
@@ -1,1 +0,0 @@
-docker build -t symbol-rest-local .
--- yarn_setup.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/sh
-
-# install servers
-for module in 'catapult-sdk' 'rest' 'spammer' 'tools'
-do
-	cd "${module}"
-	yarn install
-	cd ..
-done
